3d5be59245bbf25fca9f6c3709009efb72152c75
Xiao Han <156427197+summerhare12@users.noreply.github.com>
2024-09-25 10:14:15 +0800

    Add files via upload

--- chicken/chicken.cpp
+++ chicken/chicken.cpp
@@ -50,10 +50,6 @@ class Chicken
             return age;
         }
 
-        void printChicken()//I've added this function to print the chicken's name and age  :)
-        {
-            std::cout << "Hi, everyone! My name is " << name << ", I am " << age << " years old." << std::endl;
-        }
 
         Chicken( const Chicken &chick )//I've written this function!! This function is a copy constructor ~
         {
@@ -78,15 +74,27 @@ class Chicken
         }
 };
 
-int main()
-{
-    Chicken c(24, "Kunkun");
-    c.printChicken();
-    Chicken c1 = c;
-    c1.printChicken();// to test the copy constructor
-    Chicken c2;
-    c2 = c1= c;
-    c2.printChicken();// to test the copy assignment operator
+int main(){
+auto print = [](const Chicken &c){
+std::cout << "Hi, everyone! My name is " << c.getName()
+<< ", I am " << c.getAge() << " years old." << std::endl;
+};
+Chicken c(24, "Kunkun");
+print(c);
+Chicken d;
+d = c;
+print(d); // 测试【赋值运算符】是否正确，能正确输出给 20 分
+Chicken a = c;
+print(a); // 测试【赋值构造函数】是否正确，能正确输出给 20 分
+c.setName("Xukun Cai");
+print(c);
+print(a);
+print(d); // 测试是否为【深度复制】，本行与上两行能正确输出给 20 分
+Chicken b;
+b = d = c;
+print(b);
+print(d); // 测试【连续赋值】功能，本行与上一行能正确输出给 20 分
+return 0;
 }
 // i've also used the Valgrind to check the memory leaks. Luckily, this code is fine.
 // Here is the result of Valgrind:
